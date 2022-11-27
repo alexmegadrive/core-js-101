53ec69f83dd81f57caf6630ae0d58c382f4b46a2
Alexander Megadrive <alexmihailov91@gmail.com>
2022-11-27 13:56:38 +0300

    init

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,10 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(w, h) {
+  this.width = w;
+  this.height = h;
+  this.getArea = () => this.width * this.height;
 }
 
 
