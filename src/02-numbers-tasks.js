9c26c3fb0b0611e0758ad12dc0673bca086bdbfe
Alexander Megadrive <alexmihailov91@gmail.com>
2023-01-03 01:42:00 +0300

    feat: add 2 solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -73,8 +73,8 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints( /* x1, y1, x2, y2 */ ) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
 /**
@@ -89,8 +89,8 @@ function getDistanceBetweenPoints( /* x1, y1, x2, y2 */ ) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot( /* a, b */ ) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 
@@ -185,8 +185,8 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen( /* num, pow */ ) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
